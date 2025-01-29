ef65ecdbbb1a8f5e20927cae05ea22e90abb9dbc
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2025-01-29 14:18:23 +0200

    attempts to fix the cmake config for tests

--- test/http/http1.0.cpp
+++ test/http/http1.0.cpp
@@ -1,14 +1,43 @@
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
+#include <gmock/gmock.h>
+
+// Mock class
+class MockExample
+{
+public:
+	MOCK_METHOD(int, add, (int, int), ());
+	MOCK_METHOD(int, subtract, (int, int), ());
+};
+
+TEST_CASE("Test add function with Google Mock", "[add]")
+{
+	MockExample mock;
+
+	// Expect mock to be called with specific arguments
+	EXPECT_CALL(mock, add(2, 3)).WillOnce(testing::Return(5));
+
+	// Validate behavior
+	CHECK(mock.add(2, 3) == 5);
+}
+
+TEST_CASE("Test subtract function with Google Mock", "[subtract]")
+{
+	MockExample mock;
+
+	// Expect mock to be called with specific arguments
+	EXPECT_CALL(mock, subtract(5, 3)).WillOnce(testing::Return(2));
+
+	// Validate behavior
+	CHECK(mock.subtract(5, 3) == 2);
+}
+
 #include <protocol_handler/http/1.0.h>
 
-TEST(HTTP10, ParsesWholeRequestProperly)
+TEST_CASE("HTTP 1.0", "[parse whole request properly]")
 {
 	protocol::HTTP1 agent;
 	auto parser = agent.get_praser();
-}
 
-int main(int argc, char** argv)
-{
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	// Validate behavior
+	CHECK(2 == 2);
 }
